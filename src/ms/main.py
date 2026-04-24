50d37c15c51877839f52f5f776e784f16caa028e
Brody King <46272894+brodyking@users.noreply.github.com>
2026-04-23 21:37:39 -0500

    Not needed

--- src/ms/main.py
+++ src/ms/main.py
@@ -3,21 +3,6 @@ from ms.database import Database
 from pathlib import Path
 import argparse
 
-def load_config():
-    # Config path
-    config_path = Path.home() / "Music" / "ms" / "config.json"
-    # Creates directory if it dosen't exist
-    config_path.parent.mkdir(parents=True, exist_ok=True)
-    # Create file with defaults if it dosent exist
-    if not config_path.exists():
-        default_config = {"test key": "test value"}
-        config = default_config or {}
-        config_path.write_text(json.dumps(config, indent=2))
-        return config
-    with config_path.open() as f:
-        return json.load(f)
-
-
 def main():
     db = Database()
 
