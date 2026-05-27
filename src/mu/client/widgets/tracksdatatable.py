64e75c448200b5461999ceaf1140f6a41f22a9ee
Brody King <bk.2k@hotmail.com>
2026-05-26 23:33:40 -0500

    Moved VimDataTable to separate file

--- src/mu/client/widgets/tracksdatatable.py
+++ src/mu/client/widgets/tracksdatatable.py
@@ -1,22 +1,6 @@
 from textual.app import ComposeResult
 from textual.widgets import DataTable, Input, Static
-
-
-# A separate class to wrap the table and other widgets (like search)
-class VimDataTable(DataTable):
-    BINDINGS = [
-        ("j", "cursor_down", "Down"),
-        ("k", "cursor_up", "Up"),
-    ]
-
-    CSS = """
-    VimDataTable {
-        width: auto;
-        padding: 0;
-        margin: 0;
-    }
-    """
-
+from mu.client.widgets.vimdatatable import VimDataTable
 
 class TracksDataTable(Static):
     BINDINGS = [("/", "focus_search", "Search")]
