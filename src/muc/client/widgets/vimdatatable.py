127471bf1db57edf5c91252bf7a0916d33ba0d5a
Brody King <bk.2k@hotmail.com>
2026-06-19 22:08:56 -0500

    Tab now inspects a track, showing its metadata in a horizontal table

--- src/muc/client/widgets/vimdatatable.py
+++ src/muc/client/widgets/vimdatatable.py
@@ -7,7 +7,57 @@
 
 """
 
+from textual import on
+from textual.app import ComposeResult
+from textual.coordinate import Coordinate
+from textual.screen import ModalScreen
 from textual.widgets import DataTable
+
+
+class InspectRow(ModalScreen[str]):
+    DEFAULT_CSS = """
+    InspectRow {
+        /* Forces everything inside the modal screen to center perfectly */
+        align: center middle;
+
+        /* The alpha percentage allows the underlying app screen to show through */
+        /*background: black 40%;*/
+        background: transparent;
+    }
+
+    VimDataTable {
+        width: 45;
+        height: auto;
+        border: heavy $primary;
+        padding: 0 0;
+        align: center middle;
+        overflow:hidden;
+    }
+
+    """
+
+    BINDINGS = [
+        ("escape", "dismiss_msg('cancel')", "Close"),
+    ]
+
+    def __init__(self, row_dict, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.main_table = VimDataTable(cursor_type="row")
+        self.row_dict = row_dict
+
+    def compose(self) -> ComposeResult:
+        yield self.main_table
+
+    def on_mount(self) -> None:
+
+        self.main_table.add_column("Key", key=0, width=9)
+        self.main_table.add_column("Value", key=1, width=100)
+
+        for label in self.row_dict.keys():
+            self.main_table.add_row(label, self.row_dict[label])
+
+    def action_dismiss_msg(self, message: str):
+        self.dismiss(message)
 
 
 # A separate class to wrap the table and other widgets (like search)
@@ -15,6 +65,7 @@ class VimDataTable(DataTable):
     BINDINGS = [
         ("j", "cursor_down", "Down"),
         ("k", "cursor_up", "Up"),
+        ("tab", "inspect", "Inspect"),
     ]
 
     CSS = """
@@ -24,3 +75,16 @@ class VimDataTable(DataTable):
         margin: 0;
     }
     """
+
+    def action_inspect(self):
+
+        col_labels = []
+        for col in self.columns:
+            col_labels.append(col.value)
+
+        row_dict = {}
+        for i, col_label in enumerate(col_labels):
+            row_dict[col_label] = self.get_cell_at(Coordinate(self.cursor_row, i))
+
+        popup = InspectRow(row_dict)
+        self.app.push_screen(popup)  # type:ignore
