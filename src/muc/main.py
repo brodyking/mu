93357ff573ee061e3fa09119c3d22e0718eeddd2
Brody King <bk.2k@hotmail.com>
2026-07-02 21:59:22 -0500

    Removed references to textual_serve

--- src/muc/main.py
+++ src/muc/main.py
@@ -9,19 +9,12 @@
 
 import argparse
 
-from textual_serve.server import Server
-
 from muc.client.client import Client
 
 
 def start_client_tui():
     client = Client()
     client.run()
-
-
-def start_client_web():
-    server = Server("muc", title="muc")
-    server.serve()
 
 
 def main():
@@ -35,11 +28,11 @@ def main():
         """,
     )
 
-    parser.add_argument("action", nargs="?", choices=["web", "tui"], default="tui")
+    parser.add_argument("action", nargs="?", choices=["tui"], default="tui")
 
     args = parser.parse_args()
 
-    actions = {"web": start_client_web, "tui": start_client_tui}
+    actions = {"tui": start_client_tui}
 
     if args.action in actions:
         actions[args.action]()
