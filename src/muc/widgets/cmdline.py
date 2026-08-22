714d25ca5812bfdbc86421231da3e090c74b06a1
Brody King <bk.2k@hotmail.com>
2026-08-22 16:00:49 -0500

    New command line autofill hints, fully working correctly

--- src/muc/widgets/cmdline.py
+++ src/muc/widgets/cmdline.py
@@ -10,19 +10,15 @@
 import os
 
 from textual import on
-from textual.containers import Horizontal, Vertical
+from textual.containers import Horizontal
 from textual.coordinate import Coordinate
-from textual.message import Message
 from textual.screen import ModalScreen
-from textual.suggester import SuggestFromList
 from textual.widgets import Input, Label
 
 from muc.widgets.vimdatatable import VimDataTable
 
 
 class CmdLineHintTable(VimDataTable):
-    BINDINGS = [("enter", "row_selected", "Select")]
-
     def __init__(self, *args, **kwargs) -> None:
         super().__init__()
         self.can_focus = False
@@ -40,14 +36,14 @@ class CmdLine(ModalScreen[str]):
         ("tab", "accept_hint", "Accept hint"),
     ]
 
-    PARSER = [
-        "scan",
-        "version",
-        "itunes",
-        "list",
-        "playlist",
-        "track",
-    ]
+    COMMAND_TREE = {
+        "scan": [],
+        "version": [],
+        "itunes": [],
+        "list": ["tracks", "albums", "artists", "playlists", "playlist"],
+        "playlist": ["append", "insert", "remove", "create", "delete"],
+        "track": ["append", "create", "insert", "remove", "delete"],
+    }
 
     def __init__(
         self, default_value: str = "", auto_submit: bool = False, *args, **kwargs
@@ -60,7 +56,7 @@ class CmdLine(ModalScreen[str]):
         self.prompt = Label("> mu")
 
         self.hint_table = CmdLineHintTable()
-
+        self.in_subcommand = False
         self.auto_submit = auto_submit
         super().__init__(*args, **kwargs)
 
@@ -91,9 +87,44 @@ class CmdLine(ModalScreen[str]):
     @on(Input.Changed)
     def update_hint_table(self, event: Input.Changed | None) -> None:
         self.hint_table.clear()
-        for i, possible in enumerate(self.PARSER):
-            if not event or (event.value.lower() == possible[: (len(event.value))]):
-                self.hint_table.add_row(possible, key=str(i))
+
+        if not event:
+            hints = list(self.COMMAND_TREE.keys())
+        else:
+            command, _, subcommand = event.value.partition(" ")
+
+            hints = []
+
+            self.in_subcommand = command in self.COMMAND_TREE
+
+            if self.in_subcommand:
+                if not subcommand:
+                    hints = self.COMMAND_TREE[command]
+                else:
+                    hints = (
+                        hint
+                        for hint in self.COMMAND_TREE[command]
+                        if subcommand in hint
+                    )
+            else:
+                if not command:
+                    hints = list(self.COMMAND_TREE.keys())
+                else:
+                    hints = (
+                        hint
+                        for hint in self.COMMAND_TREE.keys()
+                        if command == hint[: len(command)]
+                    )
+
+        for hint in hints:
+            self.hint_table.add_row(hint)
+
+    # @on(Input.Changed)
+    # def update_hint_table(self, event: Input.Changed | None) -> None:
+    #     self.hint_table.clear()
+    #     for i, possible in enumerate(self.BASE_PARSER):
+    #         if not event or (event.value.lower() == possible[: (len(event.value))]):
+    #             self.hint_table.add_row(possible, key=str(i))
 
     def action_focus_hint_previous(self) -> None:
         self.hint_table.action_cursor_up()
@@ -103,9 +134,13 @@ class CmdLine(ModalScreen[str]):
 
     def action_accept_hint(self) -> None:
         try:
-            self.search.value = self.hint_table.get_cell_at(
+            hint = self.hint_table.get_cell_at(
                 Coordinate(self.hint_table.cursor_row, 0)
             )
+            if self.in_subcommand:
+                self.search.value = f"{self.search.value.split(' ')[0]} {hint}"
+            else:
+                self.search.value = hint
             self.search.action_end()
         except Exception:
             ...
