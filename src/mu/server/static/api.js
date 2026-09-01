2733bad129bacdc699c48ebf8eb250ee1cefa4b2
Brody King <bk.2k@hotmail.com>
2026-09-01 18:27:20 -0500

    Added auto-col for albums

--- src/mu/server/static/api.js
+++ src/mu/server/static/api.js
@@ -34,6 +34,9 @@ const getTracksFavorites = async (q) => {
 // Get albums
 const getAlbums = async (q) => {
   if (q == undefined) { q = "" }
+  if (q.length !== 0 && q.indexOf("%3A") == -1 && q.indexOf("%3D") == -1) {
+    q = `albumartist:${q},album:${q}`
+  }
   const response = await fetch(`/api/albums?q=${q}`)
   const data = await response.json()
   return data;
