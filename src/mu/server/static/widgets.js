16be9acf88918437433ad44afc3893ca2f435b31
Brody King <bk.2k@hotmail.com>
2026-09-01 14:48:41 -0500

    Fixed wrap + alt text hovering

--- src/mu/server/static/widgets.js
+++ src/mu/server/static/widgets.js
@@ -11,19 +11,19 @@ const TracksTable = async (data) => {
   data.forEach(track => {
     rows += `
     <tr>
-        <td>`+ track.id + `</td>
-        <td>`+ (track.favorite ? `❤️` : " ") + `</td>
-        <td>`+ track.title + `</td>
-        <td>`+ track.artist + `</td>
-        <td>`+ track.album + `</td>
-        <td>`+ track.plays + `</td>
-        <td>`+ track.time + `</td>
-        <td>`+ track.dateadded + `</td>
-        <td>`+ track.tracknumber + `</td>
-        <td>`+ track.albumartist + `</td>
-        <td>`+ track.discnumber + `</td>
-        <td>`+ track.genre + `</td>
-        <td>`+ track.date + `</td>
+        <td title="${track.id}">${track.id}</td>
+        <td title="${track.favorite ? "Favorite" : "Not Favorited"}">` + (track.favorite ? `❤️` : " ") + `</td>
+        <td title="${track.title}">${track.title}</td>
+        <td title="${track.artist}">${track.artist}</td>
+        <td title="${track.album}">${track.album}</td>
+        <td title="${track.plays}">${track.plays}</td>
+        <td title="${track.time}">${track.time}</td>
+        <td title="${track.dateadded}">${track.dateadded}</td>
+        <td title="${track.tracknumber}">${track.tracknumber}</td>
+        <td title="${track.albumartist}">${track.albumartist}</td>
+        <td title="${track.discnumber}">${track.discnumber}</td>
+        <td title="${track.genre}">${track.genre}</td>
+        <td title="${track.date}">${track.date}</td>
       </tr >
     `
   })
