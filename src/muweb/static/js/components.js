05168f6f1402e99b74efefb67f5e7a7002d8379d
Brody King <bk.2k@hotmail.com>
2026-09-16 12:19:39 -0500

    Highlight all text in search field on click

--- src/muweb/static/js/components.js
+++ src/muweb/static/js/components.js
@@ -194,7 +194,7 @@ const TracksTable = (onlyFavorites = false) => {
   // Search
   search.className = "search-form"
   search.innerHTML = `
-    <input type="text" name="term" autocomplete="off" id="tracks-table-search" class="form-control rounded-0 w-100 border-0 border-bottom border-light-gray shadow-none" placeholder="Search tracks..."/>
+    <input type="text" name="term" autocomplete="off" id="tracks-table-search" class="form-control rounded-0 w-100 border-0 border-bottom border-light-gray shadow-none" placeholder="Search tracks..." onfocus="this.select()"/>
   `;
   search.addEventListener('submit', async function(event) {
     // Listens for search submit, fetches tracks and updates rows.
